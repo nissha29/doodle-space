fdd8667dd3e51c0fbd8f678c8ec37216d1ecc238
nissha29 <nishakashyap2907@gmail.com>
2025-06-29 14:30:12 +0530

    add common package for zod validations

--- apps/http-server/src/controllers/user.controller.ts
+++ apps/http-server/src/controllers/user.controller.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from "express"
 import jwt from 'jsonwebtoken'
+import { CreateUserSchema, signInSchema } from '@repo/common/types'
 
 const signup = (req: Request, res: Response) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(! data.success){
+        return res.json({
+            message: `Incorrect inputs`
+        })
+    }
+
     res.json({
         message: `User Signed up`
     })
 }
 
 const signin = (req: Request, res: Response) => {
+    const data = signInSchema.safeParse(req.body);
+    if(! data.success){
+        return res.json({
+            message: `Incorrect inputs`
+        })
+    }
+
     const userId = 'something';
     const secret = process.env?.JWT_SECRET || '';
 

fcab0b46a47217177950895b040d0bcc3b541e67
nissha29 <nishakashyap2907@gmail.com>
2025-08-11 16:41:53 +0530

    fixed UI

--- apps/client/src/components/hero/Features.tsx
+++ apps/client/src/components/hero/Features.tsx
@@ -71,28 +71,28 @@ const Feature = ({
   return (
     <div
       className={cn(
-        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
-        (index === 0 || index === 3) && "lg:border-l dark:border-neutral-800",
-        index < 3 && "lg:border-b dark:border-neutral-800"
+        "flex flex-col lg:border-r  py-10 relative group/feature border-neutral-800",
+        (index === 0 || index === 3) && "lg:border-l border-neutral-800",
+        index < 3 && "lg:border-b border-neutral-800"
       )}
       id="features"
     >
       {index < 3 && (
-        <div className="opacity-0 group-hover/feature:opacity-50 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-300 dark:from-neutral-900 to-transparent pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-50 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-900 to-transparent pointer-events-none" />
       )}
       {index >= 3 && (
-        <div className="opacity-0 group-hover/feature:opacity-50 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-300 dark:from-neutral-900 to-transparent pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-50 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-900 to-transparent pointer-events-none" />
       )}
       <div className="mb-4 relative z-10 px-10 text-cyan-300">
         {icon}
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-cyan-300 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100 text-xl tracking-wider">
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-700 group-hover/feature:bg-cyan-300 transition-all duration-200 origin-center" />
+        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-100 text-xl tracking-wider">
           {title}
         </span>
       </div>
-      <p className="text-neutral-700 dark:text-neutral-300 max-w-xs relative z-10 px-10 tracking-wider">
+      <p className="text-neutral-300 max-w-xs relative z-10 px-10 tracking-wider">
         {description}
       </p>
     </div>
